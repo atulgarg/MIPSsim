7563d6c38cc401172285e46c3d31614a04408073
atulgarg <atgarg@cise.ufl.edu>
2014-11-22 15:40:42 -0500

    removing Assembler.h since renamed to Simulator

--- Assembler.h
+++
@@ -1,8 +0,0 @@
-#include "Pipeline.h"
-class Assembler{
-        Pipeline pipeline;
-        map<int,Abstract*>* memory_map;
-        int program_counter; 
-        public:
-        Assembler(map<int,Abstract*>* memory_map,int program_counter);
-};
